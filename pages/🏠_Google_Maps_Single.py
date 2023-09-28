7b955c1ec82507d45882b2c3396937b64cfcfe06
Paul Renger <paulrenger99@gmail.com>
2023-09-28 11:35:22 +0200

    Refactoring

--- pages/🏠_Google_Maps_Single.py
+++ pages/🏠_Google_Maps_Single.py
@@ -21,7 +21,7 @@ import os
 
 from patchify import patchify
 
-from utils import get_model_from_gcs
+from utils import get_model_from_gcs, compute_iou, dim_dict, LOCAL_API_DATA_FOLDER, MAPS_API_KEY
 
 
 st.set_page_config(
@@ -30,8 +30,6 @@ st.set_page_config(
     page_icon="🏛️",)
 
 
-LOCAL_API_DATA_FOLDER = ""
-MAPS_API_KEY = st.secrets["MAPS_API_KEY"]
 
 
 
@@ -46,11 +44,7 @@ show_iou = st.sidebar.checkbox('Show IOU graph')
 
 # The predict button comes after the definition of the predict function
 
-dim_dict = {
-    "unet":(200,200,3),
-    "segnet":(200,200,3),
-    "DeepLabV3":(512,512,3)
-}
+
 
 
 
@@ -320,28 +314,3 @@ def get_ground_truth(lat, lon, zoom=17, dimensions = (200,200, 3)):
     result[mask] = (255,255,255)
 
     return result
-
-def compute_iou(mask1, mask2):
-    """
-    Compute the Intersection over Union (IoU) of two binary segmentation masks.
-
-    Args:
-        mask1 (numpy.ndarray): First binary mask.
-        mask2 (numpy.ndarray): Second binary mask.
-
-    Returns:
-        float: IoU score.
-    """
-    # Ensure the masks are binary
-    mask1 = (mask1 > 0).astype(np.uint8)
-    mask2 = (mask2 > 0).astype(np.uint8)
-
-    # Intersection and Union
-    intersection = np.logical_and(mask1, mask2).sum()
-    union = np.logical_or(mask1, mask2).sum()
-
-    # Avoid division by zero
-    if union == 0:
-        return 0.0
-
-    return intersection / union
--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ from google.oauth2 import service_account
 import streamlit as st
 import tensorflow as tf
 
+
+LOCAL_API_DATA_FOLDER = ""
+MAPS_API_KEY = st.secrets["MAPS_API_KEY"]
+
+dim_dict = {
+    "unet":(200,200,3),
+    "segnet":(200,200,3),
+    "DeepLabV3":(512,512,3)
+}
+
 @st.cache_resource
 def get_model_from_gcs(model="unet"):
 
@@ -29,3 +39,28 @@ def get_model_from_gcs(model="unet"):
     model = tf.keras.models.load_model("model")
 
     return model
+
+def compute_iou(mask1, mask2):
+    """
+    Compute the Intersection over Union (IoU) of two binary segmentation masks.
+
+    Args:
+        mask1 (numpy.ndarray): First binary mask.
+        mask2 (numpy.ndarray): Second binary mask.
+
+    Returns:
+        float: IoU score.
+    """
+    # Ensure the masks are binary
+    mask1 = (mask1 > 0).astype(np.uint8)
+    mask2 = (mask2 > 0).astype(np.uint8)
+
+    # Intersection and Union
+    intersection = np.logical_and(mask1, mask2).sum()
+    union = np.logical_or(mask1, mask2).sum()
+
+    # Avoid division by zero
+    if union == 0:
+        return 0.0
+
+    return intersection / union
